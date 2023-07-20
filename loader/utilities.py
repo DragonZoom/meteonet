016d62fc2b2cfec427ff3508c2d5698e90c7d184
Dominique Béréziat <Dominique.Bereziat@inria.fr>
2023-07-20 18:55:15 +0200

    remove obsolets function + get_item_by_date

--- loader/utilities.py
+++ loader/utilities.py
@@ -1,39 +1,19 @@
 # Meteonet files utilities
 
-from os.path import basename, isfile, join
+from os.path import basename, isfile, join, dirname
 import numpy as np
 from tqdm import tqdm
 from glob import glob
 import torch
 
-def by_year(filename, year):
-    return filename.find(f'y{year}-') != -1
-def by_month(filename, month):
-    return filename.find(f'M{month}-') != -1
-def by_day(filename, day):
-    return filename.find(f'd{day}-') != -1
-def by_hour(filename, hour):
-    return filename.find(f'h{hour}-') != -1
-def by_minute(filename, minute):
-    return filename.find(f'm{minute}.') != -1
 def split_date(filename):
     year,month,day,hour,minute = [k[1:] for k in basename(filename).split(".")[0].split("-")]
     return int(year),int(month),int(day),int(hour),int(minute)
-def have_wind(rainmap):
-    return isfile(rainmap.replace('rainmaps',join('windmaps','U')))
-def get_wind(rainmap):
-    return rainmap.replace('rainmaps',join('windmaps','U')), rainmap.replace('rainmaps',join('windmaps','V'))
+
 def load_map(npz):
     # ->Array[int16] 
     return list(np.load(npz).values())[0]
-def load_params(npz):
-    """ load param file created by function .... """
-    obj = np.load(npz,allow_pickle=True)
-    return obj['arr_0'].reshape(-1)[0]
-def get_files(dir):
-    return sorted( glob(dir), key=lambda f:split_date(f))
-def save_params(params, file):
-    np.savez_compressed( file, params)
+
 def map_to_classes( rainmap, thresholds):
     # Array * List[float] -> Array[bool]
     # return np.array([rainmap >= th for th in thresholds])
@@ -59,24 +39,16 @@ def next_date(filename, ext='npz'):
         else: hour += 1
     else: minute += 5
     return f'y{year}-M{month}-d{day}-h{hour}-m{minute}.{ext}'
-#    return 'y'+str(year)+'-M'+str(month)+'-d'+str(day)+'-h'+str(hour)+'-m'+str(minute)+'.'+ext
 
-def create_params(rainmap_train, rainmap_val, thresholds = [1]):
-    """ determine normalisation paramerts from a train set
-        and various parameters useful for the Meteonet dataloader
-        * difference with Vincent: maps having missing values (-1) are kept because they contain valuable signal
-        * V1: only rainmaps
-        * there is a default value for thresholds
-    """
-    tq = tqdm( rainmap_train, unit=" files")
-    l = len(thresholds)
-    data = np.empty( (len(tq), l))
-    imin, imax = 1000, 0
-    for i,file in enumerate(tq):
-        mapr = load_map(file)
-        amax = mapr.max()
-        if amax > imax: imax = amax
-        classes = map_to_classes( mapr, thresholds).reshape((l,-1))
-        data[i] = np.sum(classes,axis=1)>0
-    return { 'max':imax, 'train': rainmap_train, 'val': rainmap_val,
-             'thresholds': thresholds, 'classes': data}
+def get_item_by_date(ds, date):
+    """ utility: returns the item in which date belongs to """
+    files = ds.params['files']
+    items = ds.params['items']
+        
+    y,M,d,h,m = date
+    f = f'{dirname(files[0])}/y{y}-M{M}-d{d}-h{h}-m{m}.npz'
+    if not f in files:
+        print('date not available')
+        return None
+    idx, _ = np.where(items[:,:-1]==files.index(f))
+    return idx[0]
