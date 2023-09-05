acf221f52da3e7362846a245c26ad00af20fb73c
Dominique Béréziat <Dominique.Bereziat@lip6.fr>
2023-09-05 16:02:13 +0200

    clean

--- models/unet.py
+++ models/unet.py
@@ -103,8 +103,7 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        return self.outc(x)
     
     def get_nb_params(self):
         pp=0
