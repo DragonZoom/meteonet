aa9a9ad43b3c587e51515eeaad38ef86f85ecbec
Dmitrii Drozdov <da.drozdov85@gmail.com>
2025-01-28 08:08:48 +0100

    [FsrGAN][Training] Implement separate training for first stage1
    Use normalize turget with denormalization before thresholding.

---
+++ train-gan-stage1.py
@@ -0,0 +1,242 @@
+#
+# Difference from train-reg is that stage1 predict sequence of radar images, 
+# while train-reg predict a single radar image.
+#
+
+import argparse, torch, os
+from platform import processor  # for M1/M2 support
+
+parser = argparse.ArgumentParser(
+    prog="train-reg", description="Traning a UNet for Meteonet nowforecast"
+)
+
+parser.add_argument(
+    "-dd",
+    "--data-dir",
+    type=str,
+    help="Directory containing data",
+    dest="data_dir",
+    default="data",
+)
+parser.add_argument(
+    "-t",
+    "--thresholds",
+    type=float,
+    nargs="+",
+    help="Rainmap thresholds in mm/h (used by binary metrics)",
+    dest="thresholds",
+    default=[0.1, 1, 2.5],
+)
+parser.add_argument(
+    "-Rd",
+    "--run-dir",
+    type=str,
+    help="Directory to save logs and checkpoints",
+    dest="run_dir",
+    default="runs",
+)
+parser.add_argument(
+    "-e", "--epochs", type=int, help="Number of epochs", dest="epochs", default=20
+)
+parser.add_argument(
+    "-b", "--batch-size", type=int, help="Batch size", dest="batch_size", default=128
+)
+parser.add_argument(
+    "-lrg1",
+    "--learning-rate-g1",
+    type=float,
+    help="LR_WD format is: epoch:Learning rate, weight decay",
+    dest="lr_g1",
+    default=0.0005,
+)
+parser.add_argument(
+    "-nw",
+    "--num-workers",
+    type=int,
+    help="Numbers of workers for Cuda",
+    dest="num_workers",
+    default=8 if processor() != "arm" else 0,
+)  # no multithreadings on M1/M2 :(
+parser.add_argument(
+    "-o",
+    "--oversampling",
+    type=float,
+    help="Oversampling percentage of last class",
+    dest="oversampling",
+    default=0.9,
+)
+parser.add_argument(
+    "-ss", "--snapshot-step", type=int, help="", dest="snapshot_step", default=5
+)
+parser.add_argument("-db", "--debug", type=bool, help="", dest="debug", default=False)
+# models: FsrGAN, FsrGAN_no_wind
+parser.add_argument("-m", "--model", type=str, help="", dest="model", default="FsrGAN")
+args = parser.parse_args()
+
+## user parameters
+input_len = 12
+time_horizon = 6
+stride = input_len
+clip_grad = 0.1
+
+thresholds = [
+    100 * k / 12 for k in args.thresholds
+]  #  unit: CRF over 5 minutes in 1/100 of mm (as meteonet data)
+model_size = 8  # to do
+lr_wd_g1 = {0: (args.lr_g1, 1e-4)}
+
+if torch.backends.cuda.is_built() and torch.cuda.is_available():
+    device = "cuda"
+elif torch.backends.mps.is_built():
+    device = "mps"
+else:
+    device = "cpu"
+
+print(
+    f"""
+Data params:
+   {input_len = } (history of {12*5} minutes)
+   {time_horizon = } (nowcasting at {time_horizon*5} minutes)
+   {stride = }
+   model = FsrGAN regression
+   model_size = ?
+   {args.data_dir = }
+   {len(thresholds)} classes ({thresholds=})
+   
+Train params:
+   {args.epochs = } 
+   {args.batch_size = }
+   {lr_wd_g1 = }
+   {clip_grad = }
+
+Others params:
+   {device = }
+   {args.snapshot_step = }
+   {args.num_workers = }
+   {args.run_dir = }
+   {args.debug = }
+"""
+)
+
+device = torch.device(device)
+
+# model
+from models.FsrGAN import (
+    FirstStage,
+)
+from trainers.gan import train_meteonet_gan
+from datetime import datetime
+
+use_window = True
+
+if args.model == "FsrGAN_no_wind":
+    from models.FsrGAN_no_wind import RadarFirstStage
+    model1_g = RadarFirstStage(input_len, time_horizon)
+    use_window = False
+
+elif args.model == "FsrGAN":
+    model1_g = FirstStage(input_len, time_horizon, 4)
+else:
+    raise ValueError(f"Unknown model {args.model}")
+
+print(f'Use window: {use_window}')
+
+# data
+from meteonet.loader import MeteonetDatasetChunked
+from meteonet.samplers import meteonet_random_oversampler, meteonet_sequential_sampler
+from torch.utils.data import DataLoader
+from os.path import join
+
+
+train_ds = MeteonetDatasetChunked(
+    args.data_dir,
+    "train" if not args.debug else "val_small",  # limit the number of files for testing
+    input_len,
+    input_len + time_horizon,
+    stride,
+    target_is_one_map=False,
+    use_wind=use_window,
+)
+val_ds = MeteonetDatasetChunked(
+    args.data_dir,
+    "val" if not args.debug else "val_small",
+    input_len,
+    input_len + time_horizon,
+    stride,
+    target_is_one_map=False,
+    use_wind=use_window,
+)
+
+val_ds.norm_factors = train_ds.norm_factors
+
+# samplers for dataloaders
+train_sampler = meteonet_random_oversampler(train_ds, thresholds[-1], args.oversampling)
+val_sampler = meteonet_sequential_sampler(val_ds)
+
+# dataloaders
+train_loader = DataLoader(
+    train_ds,
+    args.batch_size,
+    sampler=train_sampler,
+    num_workers=args.num_workers,
+    pin_memory=True,
+)
+val_loader = DataLoader(
+    val_ds,
+    args.batch_size,
+    sampler=val_sampler,
+    num_workers=args.num_workers,
+    pin_memory=True,
+)
+
+
+print(
+    f"""
+size of train items/batch
+      {len(train_ds)} {len(train_loader)}
+size of val items/batch
+      {len(val_ds)} {len(val_loader)}
+"""
+)
+
+
+## Model & training procedure
+from trainers.gan import train_meteonet_gan_stage1
+from datetime import datetime
+
+# try:
+
+rundir = join(args.run_dir, f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+print(f"run files will be recorded in directory {rundir}")
+os.system(f'mkdir -p "{rundir}"')
+scores = train_meteonet_gan_stage1(
+    train_loader,
+    val_loader,
+    model1_g,
+    thresholds,
+    args.epochs,
+    lr_wd_g1,
+    args.snapshot_step,
+    rundir=rundir,
+    device=device,
+)
+
+hyperparams = {
+    "input_len": input_len,
+    "time_horizon": time_horizon,
+    "stride": stride,
+    "thresholds": thresholds,
+    "batch_size": args.batch_size,
+    # 'clip_grad': clip_grad,
+    "epochs": args.epochs,
+    "lr_wd": lr_wd_g1,
+    "oversampling": args.oversampling,
+    # 'model_size': model_size,
+    "data_dir": args.data_dir,
+}
+
+os.system(f'rm -f lastrun; ln -sf "{rundir}" lastrun')
+
+import torch
+
+torch.save({"hyperparams": hyperparams, "scores": scores}, join(rundir, "run_info.pt"))
--- trainers/gan.py
+++ trainers/gan.py
@@ -3,6 +3,7 @@
 import torch
 import torch.nn as nn
 from torch.optim import Adam
+import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
 from meteonet.utilities import calculate_CT, calculate_BS, map_to_classes
@@ -12,7 +13,7 @@ from os.path import join
 import time
 
 
-def calculate_weights(radar):
+def calculate_weights(radar, norm_factor):
     """
     Calculate the weights w(z) as defined in equation (2) based on radar echo value (1/100 mm).
 
@@ -23,14 +24,14 @@ def calculate_weights(radar):
         torch.Tensor: Weight tensor with the same shape as radar.
     """
     weights = torch.ones_like(radar)
-    weights = torch.where(radar >= 83, torch.tensor(2.0, device=radar.device), weights)
-    weights = torch.where(radar >= 833, torch.tensor(6.0, device=radar.device), weights)
-    weights = torch.where(radar >= 2030, torch.tensor(10.0, device=radar.device), weights)
-    weights = torch.where(radar >= 4000, torch.tensor(20.0, device=radar.device), weights)
-    weights = torch.where(radar >= 7000, torch.tensor(60.0, device=radar.device), weights)
+    weights = torch.where(radar >= np.log(0.833 + 1 + 1e-3) / norm_factor, torch.full_like(radar, 1.5), weights)
+    weights = torch.where(radar >= np.log(8.333 + 1 + 1e-3) / norm_factor, torch.full_like(radar, 3.0), weights)
+    weights = torch.where(radar >= np.log(20.83 + 1 + 1e-3) / norm_factor, torch.full_like(radar, 6.0), weights)
+    # weights = torch.where(radar >= 40.00, torch.full_like(radar, 4.0), weights)
+    # weights = torch.where(radar >= 70.00, torch.tensor(60.0, device=radar.device), weights)
     return weights
 
-def loss_stage1(predicted, ground_truth):
+def loss_stage1(predicted, ground_truth, norm_factor):
     """
     Compute the first stage loss as defined in equation (3).
 
@@ -41,10 +42,17 @@ def loss_stage1(predicted, ground_truth):
     Returns:
         torch.Tensor: The scalar loss value.
     """
-    _, T, _, _ = predicted.shape
-
     # Compute the weights based on ground truth radar echo values
-    weights = calculate_weights(ground_truth)
+    weights = calculate_weights(ground_truth, norm_factor)
+
+    # Exponential time weights: Emphasize future time steps
+    alpha = 1.6
+    T = predicted.shape[1]  # Total number of timesteps
+    time_weights = torch.tensor([alpha**t for t in range(T)], device=predicted.device)  # Shape: [T]
+    time_weights = time_weights / time_weights.sum()  # Normalize time weights to sum to 1
+
+    # Reshape time weights for broadcasting
+    time_weights = time_weights.view(1, T, 1, 1)  # Shape: [1, T, 1, 1]
 
     # Compute the squared difference and absolute difference
     squared_diff = (predicted - ground_truth) ** 2
@@ -52,12 +60,194 @@ def loss_stage1(predicted, ground_truth):
 
     # Compute the weighted loss for each pixel and each timestep
     weighted_loss = weights * (squared_diff + absolute_diff)
-    # print(f"{weighted_loss.max()=} {squared_diff.max()=} {absolute_diff.max()=}")
+    weighted_loss = weighted_loss * time_weights  # Apply time weights
 
-    # Average over spatial dimensions, time, and batch
-    loss = (weighted_loss / T).mean()
+    # Average over time, and batch
+    loss = weighted_loss.mean()
 
     return loss
+
+
+def train_meteonet_gan_stage1(
+    train_loader,
+    val_loader,
+    model_generator,
+    thresholds,
+    epochs,
+    lr_wd_g,
+    snapshot_step=5,
+    rundir="runs",
+    tqdm=tqdm,
+    device="cpu",
+):
+    print("Evaluation Persistence...")
+    # on garde les métriques ensemblistes.
+    # il faut ajouter la RMSE
+    CT_pers = 0
+    RMSE_pers = 0
+    N = 0
+    for batch in tqdm(val_loader):
+        persistance = batch["persistence"]
+        target = val_loader.dataset.denormalize_rainmap(batch["target"][:,-1])
+        
+        CT_pers += calculate_CT(
+            map_to_classes(persistance, thresholds), map_to_classes(target, thresholds)
+        )
+        f1_pers, bias_pers, ts_pers = calculate_BS(CT_pers, ["F1", "BIAS", "TS"])
+        RMSE_pers += ((persistance - target) ** 2).mean()
+        N += target.shape[0]
+        # break
+
+    RMSE_pers = RMSE_pers / N
+
+    writer = SummaryWriter(log_dir=rundir)
+
+    loss = nn.MSELoss()
+    loss.to(device)
+    model_generator.to(device)
+
+    optimizer_g1 = Adam(
+        model_generator.parameters(),
+        lr=lr_wd_g[0][0],
+        weight_decay=lr_wd_g[0][1],
+        betas=(0.5, 0.999),
+    )
+    scheduler_g1 = torch.optim.lr_scheduler.StepLR(optimizer_g1, step_size=1, gamma=1.0)
+
+    print("Start training...")
+    train_losses = []
+    train_losses_g1 = []
+    val_losses1 = []
+    val_f1_1, val_bias1, val_ts1 = [], [], []
+    for epoch in range(epochs):
+        # print learning rate
+        print(f"** epoch {epoch+1} learning rate: {scheduler_g1.get_last_lr()}")
+        gen1_inference_time = 0
+        #
+
+        model_generator.train()
+        train_loss_g1 = 0
+        N = 0
+        for batch in tqdm(train_loader, unit=" batches"):
+            x, y = batch["inputs"], batch["target"]
+            x, y = x.to(device), y.to(device)
+
+            # separate radar data from wind maps
+            B, T, H, W = y.shape
+            
+            #
+            # First stage generator
+            start_time = time.time()
+            #
+            fake_im_first_stage = model_generator(x).view(B, T, H, W)
+            # compute the loss for the first stage generator
+            loss_g1 = loss_stage1(fake_im_first_stage, y, train_loader.dataset.norm_factors[0])
+            optimizer_g1.zero_grad()
+            loss_g1.backward()
+            optimizer_g1.step()
+            
+            l = loss_g1.item()
+            train_losses_g1.append(l)
+            train_loss_g1 += l
+            #
+            gen1_inference_time += time.time() - start_time
+            #
+            N += x.shape[0]
+
+        scheduler_g1.step()
+
+        train_loss_g1 = train_loss_g1 / N
+        train_losses.append(train_loss_g1)
+        print(f"epoch {epoch+1} {train_loss_g1=}")
+        print(f"Generator 1 inference time: {gen1_inference_time:.4f} seconds")
+
+        model_generator.eval()
+        val_loss1 = np.zeros(T)
+        CT_pred1 = [0] * T
+        RMSE_pred1 = np.zeros(T)
+        N = 0
+        for batch in tqdm(val_loader, unit=" batches"):
+            x, y = batch["inputs"], batch["target"]
+            x, y = x.to(device), val_loader.dataset.denormalize_rainmap(y.to(device))
+            with torch.no_grad():
+                y_hat1 = model_generator(x).squeeze(2)
+                y_hat1 = val_loader.dataset.denormalize_rainmap(y_hat1)
+                # y_hat1 = y_hat1[:, -1].squeeze(1)
+
+            for i in range(y_hat1.shape[1]):
+                val_loss1[i] += loss(y_hat1[:, i], y[:, i]).item()
+                CT_pred1[i] += calculate_CT(
+                    map_to_classes(y_hat1[:, i], thresholds), map_to_classes(y[:, i], thresholds)
+                )
+                RMSE_pred1[i] += ((y[:, i] - y_hat1[:, i]) ** 2).mean()
+
+            # val_loss1 += loss(y_hat1, y).item()
+
+            # CT_pred1 += calculate_CT(
+            #     map_to_classes(y, thresholds), map_to_classes(y_hat1, thresholds)
+            # )            
+
+            # RMSE_pred1 += ((y - y_hat1) ** 2).mean()
+            N += x.shape[0]
+
+        print(f"epoch {epoch+1}")
+        for i, ct in enumerate(CT_pred1):
+            f1_pred1, bias1, ts1 = calculate_BS(ct, ["F1", "BIAS", "TS"])
+            print(f"  T={i}: {f1_pred1=} {val_loss1[i]=} {RMSE_pred1[i]=}")
+
+        RMSE_pred1 = RMSE_pred1[-1]
+        val_loss1 = val_loss1[-1]
+        CT_pred1 = CT_pred1[-1]
+
+
+
+        RMSE_pred1 = (RMSE_pred1) / N
+
+        val_loss1 /= N
+        val_losses1.append(val_loss1)
+
+        val_f1_1.append(f1_pred1)
+        val_bias1.append(bias1)
+        val_ts1.append(ts1)
+
+        print(f"  {val_loss1=} {f1_pred1=}")
+        print(f"  {f1_pers=}")
+
+        writer.add_scalar("train G1", train_loss_g1, epoch)
+        writer.add_scalar("val1", val_loss1, epoch)
+        for c in range(len(thresholds)):
+            writer.add_scalar(f"F1_C{c+1}_1", f1_pred1[c], epoch)
+            writer.add_scalar(f"TS_C{c+1}_1", ts1[c], epoch)
+            writer.add_scalar(f"BIAS_C{c+1}_1", bias1[c], epoch)
+            writer.add_scalar("RMSE1", RMSE_pred1, epoch)
+
+        if epoch % snapshot_step == snapshot_step - 1:
+            torch.save(
+                model_generator.state_dict(), join(rundir, f"model_s1_epoch_{epoch}.pt")
+            )
+
+    print(f"Optimisation is over. Model weights had been saved in {rundir}")
+    torch.save(model_generator.state_dict(), join(rundir, "model_s1_last_epoch.pt"))
+
+    return {
+        "train_losses": train_losses,
+        "train_losses_g1": train_losses_g1,
+        "val_losses1": val_losses1,
+        "val_f1_1": val_f1_1,
+        "f1_pers": f1_pers,
+        "val_bias1": val_bias1,
+        "bias_pers": bias_pers,
+        "val_ts1": val_ts1,
+        "ts_pers": ts_pers,
+        "val_rmse1": RMSE_pred1,
+        "RMSE_pers": RMSE_pers,
+    }
+
+
+###############################################################################
+#
+###############################################################################
+
 
 
 def train_meteonet_gan(
@@ -111,8 +301,10 @@ def train_meteonet_gan(
     train_losses_g1 = []
     train_losses_g2 = []
     train_losses_d = []
-    val_losses = []
-    val_f1, val_bias, val_ts = [], [], []
+    val_losses1 = []
+    val_losses2 = []
+    val_f1_1, val_bias1, val_ts1 = [], [], []
+    val_f1_2, val_bias2, val_ts2 = [], [], []
     for epoch in range(epochs):
         if epoch in lr_wd_g1:
             lr, wd = lr_wd_g1[epoch]
@@ -147,6 +339,7 @@ def train_meteonet_gan(
         gen2_inference_time = 0
         disc_real_inference_time = 0
         disc_fake_inference_time = 0
+        gen2_loss_time = 0
         #
 
         model_generator1.train()
@@ -166,17 +359,17 @@ def train_meteonet_gan(
             # First stage generator
             fake_im_first_stage = model_generator1(x).view(B, T, H, W)
             # compute the loss for the first stage generator
-            loss_g1 = loss_stage1(fake_im_first_stage, y)
-            optimizer_g1.zero_grad()
             start_time = time.time()
+            loss_g1 = loss_stage1(fake_im_first_stage, y, train_loader.dataset.norm_factors[0])
+            optimizer_g1.zero_grad()
             loss_g1.backward()
-            gen1_inference_time += time.time() - start_time
             optimizer_g1.step()
             
             fake_im_first_stage = fake_im_first_stage.detach()
             l = loss_g1.item()
             train_losses_g1.append(l)
             train_loss_g1 += l
+            gen1_inference_time += time.time() - start_time
             
             
             # Second stage generator
@@ -208,6 +401,7 @@ def train_meteonet_gan(
             disc_fake_inference_time += time.time() - start_time
 
             # compute the loss for the generator
+            start_time = time.time()
             W_LOSS_STAGE_2 = [1, 1, 1]
             yhat_fake_for_g = model_discriminator(fake_im)
             loss_g2_d   = W_LOSS_STAGE_2[0] * nn.BCELoss()(yhat_fake_for_g, torch.ones_like(yhat_fake_for_g))
@@ -221,6 +415,7 @@ def train_meteonet_gan(
             l = loss_g2.item()
             train_losses_g2.append(l)
             train_loss_g2 += l
+            gen2_loss_time += time.time() - start_time
 
             N += x.shape[0]
 
@@ -231,51 +426,79 @@ def train_meteonet_gan(
         print(f"epoch {epoch+1} {train_loss_g1=} {train_loss_g2=} {train_loss_d=}")
         print(f"Generator 1 inference time: {gen1_inference_time:.4f} seconds")
         print(f"Generator 2 inference time: {gen2_inference_time:.4f} seconds")
+        print(f"Generator 2 loss time: {gen2_loss_time:.4f} seconds")
         print(f"Discriminator real inference time: {disc_real_inference_time:.4f} seconds")
         print(f"Discriminator fake inference time: {disc_fake_inference_time:.4f} seconds")
 
         model_generator1.eval()
         model_generator2.eval()
-        val_loss = 0
-        CT_pred = 0
-        RMSE_pred = 0
+        val_loss1 = 0
+        CT_pred1 = 0
+        RMSE_pred1 = 0
+        val_loss2 = 0
+        CT_pred2 = 0
+        RMSE_pred2 = 0
         N = 0
         for batch in tqdm(val_loader, unit=" batches"):
             x, y = batch["inputs"], batch["target"]
             x, y = x.to(device), y.to(device)
             with torch.no_grad():
-                y_hat = model_generator1(x)
-                y_hat = model_generator2(x, y_hat)[:, -1].squeeze(1)
-            l = loss(y_hat, y)
-            val_loss += l.item()
-            CT_pred += calculate_CT(
+                y_hat1 = model_generator1(x)
+                y_hat = model_generator2(x, y_hat1)[:, -1].squeeze(1)
+                y_hat1 = y_hat1[:, -1].squeeze(1)
+
+            val_loss1 += loss(y_hat1, y).item()
+            val_loss2 += loss(y_hat, y).item()
+
+            CT_pred1 += calculate_CT(
+                map_to_classes(y, thresholds), map_to_classes(y_hat1, thresholds)
+            )            
+            CT_pred2 += calculate_CT(
                 map_to_classes(y, thresholds), map_to_classes(y_hat, thresholds)
             )
-            RMSE_pred += ((y - y_hat) ** 2).mean()
+
+            RMSE_pred1 += ((y - y_hat1) ** 2).mean()
+            RMSE_pred2 += ((y - y_hat) ** 2).mean()
             N += x.shape[0]
 
-        f1_pred, bias, ts = calculate_BS(CT_pred, ["F1", "BIAS", "TS"])
+        f1_pred1, bias1, ts1 = calculate_BS(CT_pred1, ["F1", "BIAS", "TS"])
+        f1_pred2, bias2, ts2 = calculate_BS(CT_pred2, ["F1", "BIAS", "TS"])
 
-        RMSE_pred = ((RMSE_pred) / N) * 0.5
+        RMSE_pred1 = ((RMSE_pred1) / N) * 0.5
+        RMSE_pred2 = ((RMSE_pred2) / N) * 0.5
 
-        val_loss /= N
-        val_losses.append(val_loss)
+        val_loss1 /= N
+        val_loss2 /= N
+        val_losses1.append(val_loss1)
+        val_losses2.append(val_loss2)
 
-        val_f1.append(f1_pred)
-        val_bias.append(bias)
-        val_ts.append(ts)
+        val_f1_1.append(f1_pred1)
+        val_bias1.append(bias1)
+        val_ts1.append(ts1)
 
-        print(f"epoch {epoch+1} {val_loss=} {f1_pred=} {f1_pers=}")
+        val_f1_2.append(f1_pred2)
+        val_bias2.append(bias2)
+        val_ts2.append(ts2)
+
+        print(f"epoch {epoch+1}")
+        print(f"  {val_loss1=} {f1_pred1=}")
+        print(f"  {val_loss2=} {f1_pred2=}")
+        print(f"  {f1_pers=}")
 
         writer.add_scalar("train G1", train_loss_g1, epoch)
         writer.add_scalar("train G2", train_loss_g2, epoch)
         writer.add_scalar("train D", train_loss_d, epoch)
-        writer.add_scalar("val", val_loss, epoch)
+        writer.add_scalar("val1", val_loss1, epoch)
+        writer.add_scalar("val2", val_loss2, epoch)
         for c in range(len(thresholds)):
-            writer.add_scalar(f"F1_C{c+1}", f1_pred[c], epoch)
-            writer.add_scalar(f"TS_C{c+1}", ts[c], epoch)
-            writer.add_scalar(f"BIAS_C{c+1}", bias[c], epoch)
-            writer.add_scalar("RMSE", RMSE_pred, epoch)
+            writer.add_scalar(f"F1_C{c+1}_1", f1_pred1[c], epoch)
+            writer.add_scalar(f"F1_C{c+1}_2", f1_pred2[c], epoch)
+            writer.add_scalar(f"TS_C{c+1}_1", ts1[c], epoch)
+            writer.add_scalar(f"TS_C{c+1}_2", ts2[c], epoch)
+            writer.add_scalar(f"BIAS_C{c+1}_1", bias1[c], epoch)
+            writer.add_scalar(f"BIAS_C{c+1}_2", bias2[c], epoch)
+            writer.add_scalar("RMSE1", RMSE_pred1, epoch)
+            writer.add_scalar("RMSE2", RMSE_pred2, epoch)
 
         if epoch % snapshot_step == snapshot_step - 1:
             torch.save(
@@ -302,13 +525,18 @@ def train_meteonet_gan(
         "train_losses_g1": train_losses_g1,
         "train_losses_g2": train_losses_g2,
         "train_losses_d": train_losses_d,
-        "val_losses": val_losses,
-        "val_f1": val_f1,
+        "val_losses1": val_losses1,
+        "val_losses2": val_losses2,
+        "val_f1_1": val_f1_1,
+        "val_f1_2": val_f1_2,
         "f1_pers": f1_pers,
-        "val_bias": val_bias,
+        "val_bias1": val_bias1,
+        "val_bias2": val_bias2,
         "bias_pers": bias_pers,
-        "val_ts": val_ts,
+        "val_ts1": val_ts1,
+        "val_ts2": val_ts2,
         "ts_pers": ts_pers,
-        "val_rmse": RMSE_pred,
+        "val_rmse1": RMSE_pred1,
+        "val_rmse2": RMSE_pred2,
         "RMSE_pers": RMSE_pers,
     }
